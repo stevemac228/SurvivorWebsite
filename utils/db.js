6929265fa33d627011b8d6fb7bb04a770ed498a8
Steven MacDonald <59424602+stevemac228@users.noreply.github.com>
2022-05-17 13:48:59 -0230

    Add files via upload

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+const MongoClient = require("mongodb").MongoClient
+const uri = "mongodb+srv://stevemac228:3323gg6886sm01@cluster0.jv2wr.mongodb.net/S42?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function connectToDB() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        let db = client.db('S42');
+        console.log("Connected successfully to mongoDB");  
+        return db;
+    } catch (err) {
+        throw err;
+    } 
+}
+
+async function getDb() {
+    return db;
+}
+
+async function closeDBConnection(){
+    try{
+        await client.close();    
+    }catch(err){
+        throw err;
+    }    
+};
+
+module.exports = {connectToDB, getDb, closeDBConnection}
